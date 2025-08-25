484a10a4c914360707348b3efb60a4e590a657cd
theycallmearpit <prinshuarpit@gmail.com>
2025-08-25 08:55:59 +0000

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,14 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+import { Home } from "lucide-react";
+
+
+
 
 const queryClient = new QueryClient();
 
@@ -13,13 +18,13 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
+     
+<Router>
+  <Routes>
+    <Route path="/" element={<Home />} />
+    {/* other routes */}
+  </Routes>
+</Router>
     </TooltipProvider>
   </QueryClientProvider>
 );
